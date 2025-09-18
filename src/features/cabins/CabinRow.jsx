41e526d4397c86ac98b0559a708cc40b7aa63013
Yohanes <yohanesdahlan@gmail.com>
2025-09-18 15:08:32 +0700

    displaying toast

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -1,4 +1,5 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
+import toast from 'react-hot-toast';
 import styled from 'styled-components';
 import { deleteCabin } from '../../services/apiCabins';
 import { formatCurrency } from '../../utils/helpers';
@@ -57,13 +58,13 @@ function CabinRow({ cabin }) {
   const { isLoading: isDeleting, mutate } = useMutation({
     mutationFn: deleteCabin,
     onSuccess: () => {
-      alert('Cabin successfully deleted');
+      toast.success('Cabin successfully deleted');
 
       queryClient.invalidateQueries({
         queryKey: ['cabins'],
       });
     },
-    onError: err => alert(err.message),
+    onError: err => toast.error(err.message),
   });
 
   return (
--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,8 +1,7 @@
-import React from 'react';
 import { Outlet } from 'react-router-dom';
-import Sidebar from './Sidebar';
-import Header from './Header';
 import styled from 'styled-components';
+import Header from './Header';
+import Sidebar from './Sidebar';
 
 const StyledAppLayout = styled.div`
   display: grid;
@@ -16,13 +15,23 @@ const Main = styled.main`
   padding: 4rem 4.8rem 6.4rem;
 `;
 
+const Container = styled.div`
+  max-width: 120rem;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  gap: 3.2rem;
+`;
+
 function AppLayout() {
   return (
     <StyledAppLayout>
       <Header />
       <Sidebar />
       <Main>
-        <Outlet />
+        <Container>
+          <Outlet />
+        </Container>
       </Main>
     </StyledAppLayout>
   );
