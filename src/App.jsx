e6ca0842ad1a0ae59f03404ec37fcec2b14cc450
Yohanes <yohanesdahlan@gmail.com>
2025-10-08 21:42:09 +0700

    authorization protecting routes

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import Settings from './pages/Settings';
 import Users from './pages/Users';
 import GlobalStyles from './styles/GlobalStyles';
 import AppLayout from './ui/AppLayout';
+import ProtectedRoute from './ui/ProtectedRoute';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -32,7 +33,13 @@ function App() {
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
-          <Route element={<AppLayout />}>
+          <Route
+            element={
+              <ProtectedRoute>
+                <AppLayout />
+              </ProtectedRoute>
+            }
+          >
             <Route index element={<Navigate replace to="dashboard" />} />
             <Route path="dashboard" element={<Dashboard />} />
             <Route path="bookings" element={<Bookings />} />
---
+++ src/ui/ProtectedRoute.jsx
@@ -0,0 +1,39 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+import { useUser } from '../features/authentication/useUser';
+import Spinner from './Spinner';
+
+const FullPage = styled.div`
+  height: 100vh;
+  background-color: var(---color-grey-50);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+function ProtectedRoute({ children }) {
+  const navigate = useNavigate();
+  // 1. load the authenticated user
+  const { isAuthenticated, isLoading } = useUser();
+
+  // 3. if there is no authenticated user, redirect to the /login
+  useEffect(
+    function () {
+      if (!isAuthenticated && !isLoading) navigate('/login');
+    },
+    [isAuthenticated, isLoading, navigate]
+  );
+
+  // 2. while loading, show a spinner
+  if (isLoading)
+    return (
+      <FullPage>
+        <Spinner />
+      </FullPage>
+    );
+  // 4. if there is a user, render the app
+  if (isAuthenticated) return children;
+}
+
+export default ProtectedRoute;
