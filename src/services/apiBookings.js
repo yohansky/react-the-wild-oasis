0f5fdba6147f9333dc5daa9e5866e1481b74a5e0
Yohanes <yohanesdahlan@gmail.com>
2025-10-23 14:18:17 +0700

    recent bookings and stays

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -49,6 +49,7 @@ export async function getBooking(id) {
 }
 
 // Returns all BOOKINGS that are were created after the given date. Useful to get bookings created in the last 30 days, for example.
+// date: ISOstring
 export async function getBookingsAfterDate(date) {
   const { data, error } = await supabase
     .from('bookings')
