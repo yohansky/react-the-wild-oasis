85f39f48bff62bc58d1909cd2e8edd379cea7ddc
Yohanes <yohanesdahlan@gmail.com>
2025-10-25 16:55:47 +0700

    displaying a line chart with recharts library

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import Spinner from '../../ui/Spinner';
 import { useCabins } from '../cabins/useCabins';
+import SalesChart from './SalesChart';
 import Stats from './Stats';
 import { useRecentBookings } from './useRecentBookings';
 import { useRecentStays } from './useRecentStays';
@@ -36,7 +37,7 @@ function DashboardLayout() {
       />
       <div>Today's activity</div>
       <div>Chart stay duation</div>
-      <div>Chart sales</div>
+      <SalesChart bookings={bookings} numDays={numDays} />
     </StyledDashboardLayout>
   );
 }
